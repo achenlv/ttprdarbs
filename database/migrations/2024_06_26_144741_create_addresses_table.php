25b877d1569dcd9e6edd71f9b7ec61097f133f54
achenlv <agris.pudans@outlook.com>
2024-06-26 15:02:41 +0000

    visu salikam un uzsakam veidot db

---
+++ database/migrations/2024_06_26_144741_create_addresses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('country', length: 255);
+            $table->string('city', length: 255);
+            $table->string('street', length: 255);
+            $table->text('additional_information')->charset('binary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
