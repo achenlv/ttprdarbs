9c7db9e4b0893d800c6c12cb3f3cb9fe9829c1e2
achenlv <agris.pudans@outlook.com>
2024-08-24 13:37:54 +0000

    tests

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
 
-                    lalals
+                    lalala - tests 
                 </div>
             </div>
         </div>
